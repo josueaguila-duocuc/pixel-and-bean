37c8707be623625550ab05aadd5a0aed21facd72
onlyjota <167268686+onlyjota@users.noreply.github.com>
2025-12-05 21:50:42 -0300

    Clase 1: GUI base con login y ventana principal

---
+++ src/cl/josueaguila/pnb/PixelAndBean.java
@@ -0,0 +1,14 @@
+package cl.josueaguila.pnb;
+
+import cl.josueaguila.pnb.gui.LoginFrame;
+
+public class PixelAndBean {
+
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+             new LoginFrame().setVisible(true);
+        });
+        }
+}
+    
